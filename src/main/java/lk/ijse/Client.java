5de940892d633d824cd6b7596a9f08f4405ff13e
Sachintha-Sandaruwan-Bandara <sachib843@gmail.com>
2024-01-06 11:40:29 +0530

    content update

--- src/main/java/lk/ijse/Client.java
+++ src/main/java/lk/ijse/Client.java
@@ -1,11 +1,11 @@
 package lk.ijse;
 /* 
     @author Sachi_S_Bandara
-    @created $DATE - $TIME 
+    @created 1/6/2024 - 11:38 AM 
 */
 
-public class Main {
+public class Client {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("client");
     }
 }
---
+++ src/main/java/lk/ijse/Server.java
@@ -0,0 +1,12 @@
+package lk.ijse;
+/* 
+    @author Sachi_S_Bandara
+    @created 1/6/2024 - 11:38 AM
+*/
+
+public class Server {
+    public static void main(String[] args)
+    {
+        System.out.println("server");
+    }
+}
